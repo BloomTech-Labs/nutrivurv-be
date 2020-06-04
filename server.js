9ce6240396f89935e5cbe6f0e42c9f0c5ab97f54
Cody Brewer <codybrewerdev@gmail.com>
2020-06-03 19:56:36 -0600

    pass context through buildContext function

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 import { GraphQLServer, PubSub } from 'graphql-yoga';
 import { resolvers, fragmentReplacements } from './resolvers/index';
 import prisma from './prisma';
-import getUserId from './utils/getUserId.js';
+// import isLoggedIn from './utils/getUserId.js';
 import passport from 'passport'
-import {buildContext} from 'graphql-passport'
+import { buildContext } from 'graphql-passport'
 
 const pubsub = new PubSub();
 
@@ -11,16 +11,14 @@ const pubsub = new PubSub();
 const server = new GraphQLServer({
   typeDefs: './schema.graphql',
   resolvers,
-  middlewares: [getUserId],
-  context: (request, response) => ({    
-      pubsub: pubsub,
-      prisma: prisma,
-      request: request,
-      authContext: buildContext(request, response)
+  // middlewares: [isLoggedIn],
 
-  }),
+  // context: (request, response) => ({    
+  context: (request) => buildContext(request, prisma, pubsub),
   fragmentReplacements,
   passport
 });
+
+
 
 export { server as default };
