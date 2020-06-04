0ac64d11598cadfd5f0a9987a61e115b29891856
Cody Brewer <codybrewerdev@gmail.com>
2020-06-03 20:54:27 -0600

    pass graphql-passport functions as an object to context

--- server.js
+++ server.js
@@ -7,18 +7,23 @@ import { buildContext } from 'graphql-passport'
 
 const pubsub = new PubSub();
 
-
 const server = new GraphQLServer({
   typeDefs: './schema.graphql',
   resolvers,
   // middlewares: [isLoggedIn],
 
-  // context: (request, response) => ({    
-  context: (request) => buildContext(request, prisma, pubsub),
+  // context: (request, response) => ({   
+  context(request) {
+    const passportAuth = buildContext({ req: request.request, res: request.response })
+    return {
+      prisma,
+      pubsub,
+      request,
+      passportAuth
+    };
+  },
   fragmentReplacements,
   passport
 });
-
-
 
 export { server as default };
